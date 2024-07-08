2f228078123330d4b627a3d5c9cebca52227db1b
KasunKVI <kkhettiarachchi10@gmail.com>
2024-07-08 17:24:42 +0530

    costomice the theme colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -16,11 +16,11 @@ export const Colors = {
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: '#ffff',
+    background: '#000000',
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: '#ffff',
+    tabIconDefault: '#ffff',
     tabIconSelected: tintColorDark,
   },
 };
