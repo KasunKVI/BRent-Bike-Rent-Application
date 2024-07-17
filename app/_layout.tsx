1830317ce60db60bedcdd811dc9eaff7dbc728de
Kasun Kavinda <kkhettiarachchi10@gmail.com>
2024-07-17 22:13:10 +0530

    implement the root directory routing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,21 @@
 import { Stack } from "expo-router";
+import {Drawer} from "expo-router/drawer";
 
 export default function RootLayout() {
   return (
     <Stack
       screenOptions={{
         headerShown: false,
-      }}
-    >
+      }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="signUp" />
+      <Stack.Screen name="forgetPassword1" />
+      <Stack.Screen name="forgetPassword2" />
+      <Stack.Screen name="newPassword" />
     </Stack>
-  );
+  )
+
 }
+
+
+
